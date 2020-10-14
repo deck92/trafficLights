041093f7d4fafd854c809bb0543f40de4f9cf37c
Dmitry Khartuev <deck92@yandex.ru>
2020-10-15 00:07:18 +0300

    first commit

---
+++ trafficLights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trafficLights
+//
+//  Created by Macbook on 14.10.2020.
+//  Copyright © 2020 tt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
