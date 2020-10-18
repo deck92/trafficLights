9c57d479b8a4c38f9258e542ef534adc77980a64
Dmitry Khartuev <deck92@yandex.ru>
2020-10-18 17:15:53 +0300

    fix version

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+enum CurrentLights {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet var redColorLabel: UIView!
@@ -15,32 +19,43 @@ class ViewController: UIViewController {
     @IBOutlet var greenColorLabel: UIView!
     @IBOutlet var toggleTextButton: UIButton!
     
+    private var currentLights = CurrentLights.red
+    private let lightsIsOn: CGFloat = 1
+    private let lightsIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        redColorLabel.layer.cornerRadius = 50
-        yellowColorLabel.layer.cornerRadius = 50
-        greenColorLabel.layer.cornerRadius = 50
+        
         toggleTextButton.layer.cornerRadius = 10
-        redColorLabel.alpha = 0.3
-        yellowColorLabel.alpha = 0.3
-        greenColorLabel.alpha = 0.3
+        redColorLabel.alpha = lightsIsOff
+        yellowColorLabel.alpha = lightsIsOff
+        greenColorLabel.alpha = lightsIsOff
+        viewWillLayoutSubviews()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redColorLabel.layer.cornerRadius = redColorLabel.frame.width / 2
+        yellowColorLabel.layer.cornerRadius = yellowColorLabel.frame.width / 2
+        greenColorLabel.layer.cornerRadius = greenColorLabel.frame.width / 2
     }
 
     @IBAction func toggleButtonPressed() {
-        let nameButton = "Next"
-        if round(100 * yellowColorLabel.alpha) / 100 == 0.3, redColorLabel.alpha == 1 {
-            yellowColorLabel.alpha = 1
-            redColorLabel.alpha = 0.3
-            toggleTextButton.setTitle(nameButton, for: .normal)
-        } else if round(100 * greenColorLabel.alpha) / 100 == 0.3, yellowColorLabel.alpha == 1 {
-            greenColorLabel.alpha = 1
-            yellowColorLabel.alpha = 0.3
-            toggleTextButton.setTitle(nameButton, for: .normal)
-        } else {
-            redColorLabel.alpha = 1
-            greenColorLabel.alpha = 0.3
-            toggleTextButton.setTitle(nameButton, for: .normal)
+        toggleTextButton.setTitle("Next", for: .normal)
+        switch currentLights {
+        case .red:
+            currentLights = .yellow
+            redColorLabel.alpha = lightsIsOn
+            greenColorLabel.alpha = lightsIsOff
+        case .yellow:
+            currentLights = .green
+            redColorLabel.alpha = lightsIsOff
+            yellowColorLabel.alpha = lightsIsOn
+        case .green:
+            currentLights = .red
+            yellowColorLabel.alpha = lightsIsOff
+            greenColorLabel.alpha = lightsIsOn
         }
+        
     }
     
 }
